dee11eaa0f2449d63cda00a10a84a60bafb65313
DESKTOP-1RC8MT9\NghiaLe <nghia.vlm.62cntt@ntu.edu.vn>
2022-05-20 16:35:49 +0700

    no message

---
+++ wp-content/uploads/cache/9c1d64006330662878cb29f97a9088fd333434df.php
@@ -0,0 +1,76 @@
+<!-- <section class="module_Abc">
+    <div class="w-1322 h-800">
+        <div class="main-content w-1322">
+            <hr>
+            <div class="w-400 h-800 float-left ">
+                <h1>The Encompass Platform Seamlessly Connectes the Supply Chain, Allowing You to:</h1>
+            </div>
+
+            <div class="center">
+                <div class="above">
+                    <div class="ha"><h2>Drive Business Growth</h2></div>
+                    <div class="para"><p>Tap into the power of predictive analytics and demand forecasting. Capitalize on revenue-generating opportunities faster. Encompass ERP captures mission-critical data across your entire operation, empowering you to bring your beverage of food business into new levels of profitability.</p></div>
+                </div>
+                <div class="below">
+                    <div class="hb"><h2>Improve Operational Efficiencies</h2></div>
+                    <div class="para"><p>Streamline processes and save money by reducing leakage with a fully integrated ERP system that works for your business.
+                        Encompass ERP makes it possible to identify where, when, and how resources can be best allocated to provide the most optimal outcome.</p></div>
+                </div>
+            </div>
+             
+            <div class="right">
+                <div class="above">
+                    <div class="hc"><h2>Leverage Real-Time Data for Better Decision-Marking</h2></div>
+                    <div class="para"><p>Accelerate business decisions with better, more accurate system-to-system data and comprehensive reporting that results in deep, actionable insights. Stay ahead of potential disruptions and maintain a finger on the pulse of your supply chain from anywhere in the world.</p></div>
+                </div>
+                <div class="below">
+                    <div class="hd"><h2>Enhance Team Dynamics & Employee Satisfaction</h2></div>
+                    <div class="para"><p>When your labor force has the tools they need to succeed, the workplace runs like a well-oiled machine. With Encompass ERP, every stakeholder across your supply chain-from purchasing to Point-of-sale-has what they need to do their jobs effectively.</p></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section> -->
+
+<section class="mod-abc">
+    <div class="container">
+        <div class="row">
+            <div class="col w-full lg:w-1/2">
+            <canvas id="myCanvas" width="400" height="13"></canvas>
+                <script>
+                    var canvas = document.getElementById('myCanvas');
+                    var context = canvas.getContext('2d');
+                    context.beginPath();       
+                    context.moveTo(0, 3);
+                    context.lineTo(160, 3);  
+                    context.lineWidth = 3;     
+                    context.strokeStyle = 'red';
+                    context.stroke();           
+                </script>
+                <div class="header text-h2-md"><h2>The Encompass Platform Seamlessly Connectes the Supply Chain, Allowing You to:</h2></div>
+            </div>
+            <div class="col w-full lg:w-1/2">
+                <div class="row">
+                    <div class="col w-full lg:w-1/2">
+                        <div class="ha text-h3-md"><h3>Drive Business Growth</h3></div>
+                        <div class="para text-h4-md"><p>Tap into the power of predictive analytics and demand forecasting. Capitalize on revenue-generating opportunities faster. Encompass ERP captures mission-critical data across your entire operation, empowering you to bring your beverage of food business into new levels of profitability.</p></div>
+                    </div>
+                    <div class="col w-full lg:w-1/2">
+                        <div class="hc text-h3-md"><h3>Leverage Real-Time Data for Better Decision-Marking</h3></div>
+                        <div class="para text-h4-md"><p>Accelerate business decisions with better, more accurate system-to-system data and comprehensive reporting that results in deep, actionable insights. Stay ahead of potential disruptions and maintain a finger on the pulse of your supply chain from anywhere in the world.</p></div>
+                    </div>
+                    <div class="col w-full lg:w-1/2">
+                        <div class="hb text-h3-md"><h3>Improve Operational Efficiencies</h3></div>
+                        <div class="para text-h4-md"><p>Streamline processes and save money by reducing leakage with a fully integrated ERP system that works for your business.
+                        Encompass ERP makes it possible to identify where, when, and how resources can be best allocated to provide the most optimal outcome.</p></div>
+                    </div>
+                    <div class="col w-full lg:w-1/2">
+                        <div class="hd text-h3-md"><h3>Enhance Team Dynamics & Employee Satisfaction</h3></div>
+                        <div class="para text-h4-md"><p>When your labor force has the tools they need to succeed, the workplace runs like a well-oiled machine. With Encompass ERP, every stakeholder across your supply chain-from purchasing to Point-of-sale-has what they need to do their jobs effectively.</p></div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>

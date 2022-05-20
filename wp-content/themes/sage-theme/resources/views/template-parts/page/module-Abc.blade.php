a0f546221555f12c1bb6e03efccf80c5b4a9a619
Charone\Sang Nguyen <nguyensang.016884669022gmail.com>
2022-05-20 16:33:45 +0700

    add

---
+++ wp-content/themes/sage-theme/resources/views/template-parts/page/module-Abc.blade.php
@@ -0,0 +1,88 @@
+<!-- <section class="module_Abc">
+<div class="container"><h3>Module: Abc</h3>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/style/app.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="wrap">
+        <div class="main-content">
+            <div class="float-left ">
+                <canvas id="myCanvas" width="400" height="10"></canvas>
+                <script>
+                    var canvas = document.getElementById('myCanvas');
+                    var context = canvas.getContext('2d');
+                    context.beginPath();       
+                    context.moveTo(0, 10);
+                    context.lineTo(279, 10);  
+                    context.lineWidth = 3;     
+                    context.strokeStyle = 'red';
+                    context.stroke();           
+                </script>
+                <h1>The Encompass Platform Seamlessly Connects the Supply Chain, Allowing You to:</h1>                
+            </div>
+            <div class="content_Phai bg-blue-200">
+                <div class="left-column">
+                    <div class="TieuDe1 text-4xl w-333"><h2>Drive Business Growth</h2></div>
+                        <div class="P1"><p>Tap into the power of predictive analytics and demand forecasting. Capitalize on revenue-generating opportunities faster. Encompass ERP captures mission-critical data across your entire operation, empowering you to bring your beverage or food business into hew levels of profitability.</p></div>   
+                    <div class="TieuDe2 text-4xl w-250"><h3>Improve Operational Efficiencies</h3></div>    
+                        <div class="P2"><p>Streamline processes and save money by reducirg leakage with a fully integrated ERP system that works for your business. Encompass ERP makes it possible to identify where. when, and how resources can be best allocated to provide the most bptimal outcome.</p></div>                     
+                </div>
+                <div class="right-column bg-yellow">
+                    <div class="TieuDe3 text-4xl w-250"><h4><b>Leverage Real-Time Data for Better Decision-Making</b></h4></div>
+                        <div class="P3"><p>Accelerate business decisions with better, more accurate system-to- system data and comprehensİve reporting that results in deep. actionable insights. Stay ahead of potential disruptions and maintain a finger on the pulse of your supply chain from anywhere in the world.</p></div>
+                    <div class="TieuDe4 text-4xl w-250"><h5>Enhance Team Dynamics & Employee</h5></div>                           
+                        <div class="P4"><p>When your labor force has the tools they need to succeed, the workplace runs like a well-oiled machine. With Encompass ERP. every stakeholder across your supply chain-from purchasing to Point-of-Sale--has what they need to do their jobs effectively.</p></div>                    
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+</div>
+</section> -->
+<section class="mod-abc">
+    <div class="container">
+        <div class="row">
+            <div class="col w-full lg:w-1/2">
+            <canvas id="myCanvas" width="400" height="10"></canvas>
+                <script>
+                    var canvas = document.getElementById('myCanvas');
+                    var context = canvas.getContext('2d');
+                    context.beginPath();       
+                    context.moveTo(0, 0);
+                    context.lineTo(160, 0);  
+                    context.lineWidth = 3;     
+                    context.strokeStyle = 'red';
+                    context.stroke();           
+                </script>
+            <div class="TieuDe text-h2-md"><h2>The Encompass Platform Seamlessly Connects the Supply Chain, Allowing You to:</h2></div>
+            </div>
+            <div class="col w-full lg:w-1/2">
+                <div class="row">
+                    <div class="col w-full lg:w-1/2 ">
+                        <div class="TieuDe1 text-h3-md"><h3>Drive Business Growth</h3></div>
+                            <div class="P1 text-h4-md"><p>Tap into the power of predictive analytics and demand forecasting. Capitalize on revenue-generating opportunities faster. Encompass ERP captures mission-critical data across your entire operation, empowering you to bring your beverage or food business into hew levels of profitability.</p></div>
+                    </div>
+                    <div class="col w-full lg:w-1/2">
+                        <div class="TieuDe2 text-h3-md"><h3>Leverage Real-Time Data for Better Decision-Making</h3></div>
+                            <div class="P2 text-h4-md"><p>Accelerate business decisions with better, more accurate system-to- system data and comprehensİve reporting that results in deep. actionable insights. Stay ahead of potential disruptions and maintain a finger on the pulse of your supply chain from anywhere in the world.</p></div>
+                    </div>
+                    <div class="col w-full lg:w-1/2">
+                        <div class="TieuDe3 text-h3-md"><h3>Improve Operational Efficiencies</h3></div>    
+                            <div class="P3 text-h4-md"><p>Streamline processes and save money by reducirg leakage with a fully integrated ERP system that works for your business. Encompass ERP makes it possible to identify where. when, and how resources can be best allocated to provide the most bptimal outcome.</p></div>
+                    </div>
+                    <div class="col w-full lg:w-1/2">
+                        <div class="TieuDe4 text-h3-md"><h3>Enhance Team Dynamics & Employee</h3></div>                           
+                            <div class="P4 text-h4-md"><p>When your labor force has the tools they need to succeed, the workplace runs like a well-oiled machine. With Encompass ERP. every stakeholder across your supply chain-from purchasing to Point-of-Sale--has what they need to do their jobs effectively.</p></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>

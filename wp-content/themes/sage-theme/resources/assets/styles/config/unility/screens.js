216cd70773970b085f16948f063abe6c235e485f
DESKTOP-1RC8MT9\NghiaLe <nghia.vlm.62cntt@ntu.edu.vn>
2022-05-23 21:58:53 +0700

    no message

--- wp-content/themes/sage-theme/resources/assets/styles/config/unility/screens.js
+++ wp-content/themes/sage-theme/resources/assets/styles/config/unility/screens.js
@@ -1,10 +1,12 @@
 const Screens = {
   'sm': '481px',
-  'md': '768px',
-  'lg': '992px',
-  'xl': '1200px',
-  '2xl': '1440px',
+  'md': '769px',
+  'lg': '1025px',
+  'xl': '1026px',
+  '2xl': '1441px',
   '2k': '2000px',
+  'xx':'1920px',
+  
   'down_xl': {'max': '1199px'},
   'down_lg': {'max': '991px'},
   'down_md': {'max': '767px'},

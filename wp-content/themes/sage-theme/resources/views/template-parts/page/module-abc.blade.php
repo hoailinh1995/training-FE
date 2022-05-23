aa98e445cab3b9a7b3bb9fcdeee49fdad56bba1d
DESKTOP-IBN3FJ0\DELL <truong.pn.62cntt@ntu.edu.vn>
2022-05-23 20:52:52 +0700

    update

--- wp-content/themes/sage-theme/resources/views/template-parts/page/module-abc.blade.php
+++ wp-content/themes/sage-theme/resources/views/template-parts/page/module-abc.blade.php
@@ -100,15 +100,17 @@
   </div>
 </section> -->
 <section class="module-abc">
+  <meta name="viewport" content="width-device-width, initial-scale=1">
   <div class="container">
     <div class="row w-100-per bg-white">
-      <div class="left col w-full lg:w-1/2">
-        <div class="the pr-10 ">
+      <div class="left col w-full lg:w-1/2 2xl:w-1/2 2xl:pl-10-per">
+        <div class="the">
           <h2>The Encompass Platform Seamlessly Connectes the Supply Chain, Allowing You to:</h2>
         </div>
       </div>
-      <div class="right col w-full lg:w-1/2">
-        <div class="trai float-left w-50-per">
+      <div class="right col w-full lg:w-1/2 2xl:w-1/2 2xl:pr-10-per">
+        <div class="row ">
+        <div class="trai col w-full md:w-1/2  ">
           <div class="a">
             <div class="drive">
               <h3>Drive Business Growth</h3>
@@ -117,7 +119,7 @@
               <p>Tap into the power of predictive analytics and demand forecasting. Capitalize on revenue-generating opportunities faster. Encompass ERP captures mission-critical data across your entire operation, empowering you to bring your beverage of food business into new levels of profitability.</p>
             </div>
           </div>
-          <div class="b pt-55 ">
+          <div class="b ">
             <div class="improve">
               <h3>Improve Operational Efficiencies</h3>
             </div>
@@ -127,8 +129,8 @@
             </div>
           </div>
         </div>
-        <div class="phai float-right w-50-per">
-          <div class="c pl-30">
+        <div class="phai col w-full md:w-1/2">
+          <div class="c">
             <div class="Leverage">
               <h3>Leverage Real-Time Data for Better Decision-Marking</h3>
             </div>
@@ -136,7 +138,7 @@
               <p>Accelerate business decisions with better, more accurate system-to-system data and comprehensive reporting that results in deep, actionable insights. Stay ahead of potential disruptions and maintain a finger on the pulse of your supply chain from anywhere in the world.</p>
             </div>
           </div>
-          <div class="d pl-30">
+          <div class="d">
             <div class="Enhance">
               <h3>Enhance Team Dynamics & Employee Satisfaction</h3>
             </div>
@@ -144,6 +146,7 @@
               <p>When your labor force has the tools they need to succeed, the workplace runs like a well-oiled machine. With Encompass ERP, every stakeholder across your supply chain-from purchasing to Point-of-sale-has what they need to do their jobs effectively.</p>
             </div>
           </div>
+        </div>
         </div>
       </div>
     </div>

e733e0e85e4caab703a63bda7243f637d8a5b7b1
Charone\Sang Nguyen <nguyensang.016884669022gmail.com>
2022-05-23 22:48:43 +0700

    add

--- wp-content/themes/sage-theme/resources/views/template-parts/page/module-Abc.blade.php
+++ wp-content/themes/sage-theme/resources/views/template-parts/page/module-Abc.blade.php
@@ -46,43 +46,56 @@
 </html>
 </div>
 </section> -->
-<section class="mod-abc">
-    <div class="container">
-        <div class="row">
-            <div class="col w-full lg:w-1/2">
-            <canvas id="myCanvas" width="400" height="10"></canvas>
-                <script>
-                    var canvas = document.getElementById('myCanvas');
-                    var context = canvas.getContext('2d');
-                    context.beginPath();       
-                    context.moveTo(0, 0);
-                    context.lineTo(160, 0);  
-                    context.lineWidth = 3;     
-                    context.strokeStyle = 'red';
-                    context.stroke();           
-                </script>
-            <div class="TieuDe text-h2-md"><h2>The Encompass Platform Seamlessly Connects the Supply Chain, Allowing You to:</h2></div>
-            </div>
-            <div class="col w-full lg:w-1/2">
-                <div class="row">
-                    <div class="col w-full lg:w-1/2 ">
-                        <div class="TieuDe1 text-h3-md"><h3>Drive Business Growth</h3></div>
-                            <div class="P1 text-h4-md"><p>Tap into the power of predictive analytics and demand forecasting. Capitalize on revenue-generating opportunities faster. Encompass ERP captures mission-critical data across your entire operation, empowering you to bring your beverage or food business into hew levels of profitability.</p></div>
-                    </div>
-                    <div class="col w-full lg:w-1/2">
-                        <div class="TieuDe2 text-h3-md"><h3>Leverage Real-Time Data for Better Decision-Making</h3></div>
-                            <div class="P2 text-h4-md"><p>Accelerate business decisions with better, more accurate system-to- system data and comprehensİve reporting that results in deep. actionable insights. Stay ahead of potential disruptions and maintain a finger on the pulse of your supply chain from anywhere in the world.</p></div>
-                    </div>
-                    <div class="col w-full lg:w-1/2">
-                        <div class="TieuDe3 text-h3-md"><h3>Improve Operational Efficiencies</h3></div>    
-                            <div class="P3 text-h4-md"><p>Streamline processes and save money by reducirg leakage with a fully integrated ERP system that works for your business. Encompass ERP makes it possible to identify where. when, and how resources can be best allocated to provide the most bptimal outcome.</p></div>
-                    </div>
-                    <div class="col w-full lg:w-1/2">
-                        <div class="TieuDe4 text-h3-md"><h3>Enhance Team Dynamics & Employee</h3></div>                           
-                            <div class="P4 text-h4-md"><p>When your labor force has the tools they need to succeed, the workplace runs like a well-oiled machine. With Encompass ERP. every stakeholder across your supply chain-from purchasing to Point-of-Sale--has what they need to do their jobs effectively.</p></div>
-                    </div>
-                </div>
-            </div>
+<section class="module-abc">
+  <meta name="viewport" content="width-device-width, initial-scale=1">
+  <div class="container">
+    <div class="row w-100-per bg-white">
+      <div class="left col w-full lg:w-1/2 2xl:w-1/2 2xl:pl-10-per">
+        <div class="the">
+          <h2>The Encompass Platform Seamlessly Connectes the Supply Chain, Allowing You to:</h2>
         </div>
+      </div>
+      <div class="right col w-full lg:w-1/2 2xl:w-1/2 2xl:pr-10-per">
+        <div class="row ">
+        <div class="left col w-full md:w-1/2  ">
+          <div class="a">
+            <div class="drive">
+              <h3>Drive Business Growth</h3>
+            </div>
+            <div class="tap">
+              <p>Tap into the power of predictive analytics and demand forecasting. Capitalize on revenue-generating opportunities faster. Encompass ERP captures mission-critical data across your entire operation, empowering you to bring your beverage of food business into new levels of profitability.</p>
+            </div>
+          </div>
+          <div class="b ">
+            <div class="improve">
+              <h3>Improve Operational Efficiencies</h3>
+            </div>
+            <div class="stream">
+              <p>Streamline processes and save money by reducing leakage with a fully integrated ERP system that works for your business.
+                Encompass ERP makes it possible to identify where, when, and how resources can be best allocated to provide the most optimal outcome.</p>
+            </div>
+          </div>
+        </div>
+        <div class="phai col w-full md:w-1/2">
+          <div class="c">
+            <div class="Leverage">
+              <h3>Leverage Real-Time Data for Better Decision-Marking</h3>
+            </div>
+            <div class="Accelebrate">
+              <p>Accelerate business decisions with better, more accurate system-to-system data and comprehensive reporting that results in deep, actionable insights. Stay ahead of potential disruptions and maintain a finger on the pulse of your supply chain from anywhere in the world.</p>
+            </div>
+          </div>
+          <div class="d">
+            <div class="Enhance">
+              <h3>Enhance Team Dynamics & Employee Satisfaction</h3>
+            </div>
+            <div class="when">
+              <p>When your labor force has the tools they need to succeed, the workplace runs like a well-oiled machine. With Encompass ERP, every stakeholder across your supply chain-from purchasing to Point-of-sale-has what they need to do their jobs effectively.</p>
+              </div>
+          </div>
+        </div>
+        </div>
+      </div>
     </div>
+  </div>
 </section>

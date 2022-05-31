76603422a0a1ac59760869c3e91ea5a5668cd4ab
Charone\Sang Nguyen <nguyensang.016884669022gmail.com>
2022-05-31 13:16:29 +0700

    add

--- wp-content/themes/sage-theme/resources/views/template-parts/page/module-TheLatest.blade.php
+++ wp-content/themes/sage-theme/resources/views/template-parts/page/module-TheLatest.blade.php
@@ -6,7 +6,8 @@
             </div>
             <div class="flex flex-row flex-wrap ">
                 <div class="left w-full lg:w-1/2  p-4">
-                    <div class=" bg-purple-500 rounded-3xl">
+                    <div class=" bg-white rounded-3xl shadow-2xl">
+                        <!-- <div class="bg-[url('http://localhost:3000/wp-content/themes/sage-theme/dist/images/May27.jpg')] rounded-3xl"></div> -->
                         <img class="rounded-t-3xl" src="@asset('images/May27.jpg')">
                         <div class="p-8">
                             <div class="font-bold">
@@ -15,9 +16,7 @@
                                 </h3>
                             </div>
                             <div class="font-light">
-                                <p>
                                     Lorem ipsum dolor sit amet, conseyetur sadipscing elit, sed diam nonumy eirmod.
-                                </p>
                                 <div class="font-bold text-red-700">
                                     READ MORE<span class="icomoon icon-chevron-right"></span>
                                 </div>
@@ -27,23 +26,19 @@
                 </div>
                 <div class="right flex-col flex-wrap w-full lg:w-1/2">
                     <div class="up w-full md:w-full p-4">
-                        <div class=" rounded-3xl p-8 bg-red-500">
+                        <div class=" rounded-3xl p-8 bg-white shadow-2xl">
                             <div class="font-bold">
                                 <h3>
                                     Lorem ipsum dolor sit amet, conseyetur
                                 </h3>
                             </div>
                             <div class="font-light">
-                                <p>
                                     Lorem ipsum dolor sit amet, conseyetur sadipscing elit, sed diam nonumy eirmod.
-                                </p>
-                            </div>
-                            <div class="more" style="display: none;">
-                                <div class="font-light">
-                                    <p>
-                                    Lorem ispum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod, sed diam nonumy eirmod Lorem ipsum dolor sit anet consetetur sadipscing elitr, sed diam nonumy eirmod. Lorem ipsum dolir sit amet, consetetur.
-                                    </p>
-                                </div>
+                                    <div class="more" style="display: none;">
+                                        <div class="font-light">
+                                            Lorem ispum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod, sed diam nonumy eirmod Lorem ipsum dolor sit anet consetetur sadipscing elitr, sed diam nonumy eirmod. Lorem ipsum dolir sit amet, consetetur.
+                                        </div>
+                                    </div>
                             </div>
                             <div class="flip font-bold text-red-700">
                                 READ MORE<span class="icomoon icon-chevron-right"></span>
@@ -51,17 +46,15 @@
                         </div>
                     </div>
                     <div class="down w-full md:w-full p-4">
-                        <div class="bg-green-500 rounded-3xl p-8">
+                        <div class="bg-white rounded-3xl p-8 shadow-2xl">
                             <div class="font-bold">
                             <h3>
                                 Lorem ipsum dolor sit amet, conseyetur
                             </h3>
                         </div>
                         <div class="font-light">
-                            <p>
                                 Lorem ipsum dolor sit amet, conseyetur sadipscing elit, sed diam nonumy eirmod.
-                                <span class="icomoon icon-iconC w-1432 h-675"></span>
-                            </p>
+                                <span class="icomoon icon-iconC"></span>
                             <div class="font-bold text-red-700">
                                 READ MORE<span class="icomoon icon-chevron-right"></span>
                             </div>

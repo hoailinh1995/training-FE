a050fba4809dd126e43326f3e964c7197b3137c1
DESKTOP-1RC8MT9\NghiaLe <nghia.vlm.62cntt@ntu.edu.vn>
2022-05-31 13:16:20 +0700

    31.5

--- wp-content/uploads/cache/21d161cc0bb59e3431849b3213ae41e2218cb1b3.php
+++ wp-content/uploads/cache/21d161cc0bb59e3431849b3213ae41e2218cb1b3.php
@@ -1,12 +1,13 @@
 <section class="module-TheLatest font-nunito">
-<span class="icomoon icon-icon_C1"></span>
     <div class="container font-nunito">
+    <span class="icomoon icon-iconC"></span>
         <div class="font-extrabold text-red-700">
             <p>THE LATEST</p>
         </div>
         <div class="flex flex-row flex-wrap w-full">
             <div class="left w-full lg:w-1/2 p-4 ">
-                <div class="bg-yellow rounded-3xl">
+                <div class="bg-white rounded-3xl shadow-2xl">
+                    <!-- <div class="bg-[url('')] rounded-t-3xl" w-full h-400></div> -->
                     <img class="rounded-t-3xl" src="<?= \App\asset_path('/images/May-27.jpg'); ?>">
                     <div class="p-8">
                         <div class="font-bold">
@@ -15,9 +16,7 @@
                             </h3>
                         </div>
                         <div class="font-light">
-                            <p>
                                 Lorem ispum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod.
-                            </p>
                             <div class="font-bold text-red-700">
                                 READ MORE 
                                 <span class="icomoon icon-chevron-right"></span>
@@ -28,19 +27,17 @@
             </div>
             <div class="right flex-col flex-wrap w-full lg:w-1/2">
                 <div class="up md:w-full p-4">
-                    <div class="bg-green-300 rounded-3xl p-8">
+                    <div class="bg-white rounded-3xl p-8 shadow-2xl">
                         <div class="font-bold">
                             <h3>
                                 Lorem ipsum dolor sit amet, consetetur
                             </h3>
                         </div>
                         <div class="font-light">
-                            <p>
                                 Lorem ispum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod.
                                 <div class="more" style="display: none">
                                     Lorem ispum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod. Lorem ispum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod, sed diam nonumy eirmod Lorem ipsum dolor sit anet consetetur sadipscing elitr, sed diam nonumy eirmod. Lorem ipsum dolir sit amet, consetetur.
                                 </div>
-                            </p>
                             <div class="flip font-bold text-red-700">
                                 READ MORE 
                                 <span class="icomoon icon-chevron-right"></span>
@@ -49,17 +46,15 @@
                         
                     </div>
                 </div>
-                <div class="down w-full md:w-full  p-4">
-                    <div class="bg-red-400 rounded-3xl p-8">
+                <div class="down w-full md:w-full p-4">
+                    <div class="bg-white rounded-3xl p-8 shadow-2xl">
                         <div class="font-bold">
                             <h3>
                                 Lorem ipsum dolor sit amet, consetetur
                             </h3>
                         </div>
                         <div class="font-light">
-                            <p>
                                 Lorem ispum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod.
-                            </p>
                             <div class="font-bold text-red-700">
                                 READ MORE 
                                 <span class="icomoon icon-chevron-right"></span>

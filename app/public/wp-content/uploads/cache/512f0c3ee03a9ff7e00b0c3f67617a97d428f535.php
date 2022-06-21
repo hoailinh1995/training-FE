216f6fb1f30ad2302de06495416b4491ab0dd26b
DESKTOP-1FBUQTK\LENOVO <duy.hp.62cntt@ntu.edu.vn>
2022-06-21 11:42:58 +0700

    no message

--- app/public/wp-content/uploads/cache/512f0c3ee03a9ff7e00b0c3f67617a97d428f535.php
+++ app/public/wp-content/uploads/cache/512f0c3ee03a9ff7e00b0c3f67617a97d428f535.php
@@ -1,20 +1,21 @@
 <footer id="footer" class="module footer">
-  <div class="anhfull relative">
-    <img src="<?= \App\asset_path('images/blackwave.png'); ?>" alt="hinh">
+  <div class="bg-cover md:h-400 lg:h-400 2xl:h-400 xx:h-500 w-full relative" style="background-image: url(<?= \App\asset_path('images/blackwave.png'); ?>)">
     <div class="content">
-      <div class=" absolute md:w-90 md:left-280 md:top-200"><img src="<?= \App\asset_path('images/blueen.png'); ?>" alt="hinh"></div>
-      <div class=" absolute md:w-75 md:left-290 md:top-250"><img src="<?= \App\asset_path('images/media.png'); ?>" alt="hinh"></div>
-      <div class="but text-white- border-2 border-white rounded-full"><button class="text-white">CONTACT</button></div>
+      <div class=" absolute md:w-90 md:left-280 md:top-200 lg:left-100 lg:w-200 2xl:top-220 2xl:left-160 xx:top-280"><img src="<?= \App\asset_path('images/blueen.png'); ?>" alt="hinh"></div>
+      <div class=" absolute md:w-75 md:left-290 md:top-250 lg:left-100 2xl:top-280 2xl:left-160 xx:top-340"><img src="<?= \App\asset_path('images/media.png'); ?>" alt="hinh"></div>
+      <div class="but text-white- border-2 border-white rounded-full absolute md:top-340 md:left-280 lg:top-190 lg:left-700 2xl:top-270 2xl:left-950 xx:top-330"><button class="text-white md:w-165 md:h-20 font-14">CONTACT</button></div>
       <div class="word">
-        <div class=" absolute md:top-300 md:left-130 text-white md:text-12">ORCHESTRA SITE</div>
-        <div class=" absolute md:top-300 md:left-290 text-white md:text-12">ENCOMPASS WEBSITE</div>
-        <div class=" absolute md:top-300 md:left-450 text-white md:text-12">HANDOFF WEBSITE</div>
+        <div class=" absolute md:top-300 md:left-130 text-white md:text-12 lg:top-252 lg:left-380 2xl:top-280 2xl:left-450 xx:top-330">ORCHESTRA SITE</div>
+        <div class=" absolute md:top-300 md:left-290 text-white md:text-12 lg:top-252 lg:left-560 2xl:top-280 2xl:left-600 xx:top-330">ENCOMPASS WEBSITE</div>
+        <div class=" absolute md:top-300 md:left-450 text-white md:text-12 lg:top-252 lg:left-740 2xl:top-280 2xl:left-750 xx:top-330">HANDOFF WEBSITE</div>
       </div>
-      <div class="copy">
+      <div class="copy absolute md:top-410 md:left-280 text-11 text-gray-300 lg:top-360 lg:left-95 2xl:left-160 xx:top-400">
         <p>© Encompcass Technologies 2021</p>
       </div>
     </div>
   </div>
+</footer>
+
 
 
 
